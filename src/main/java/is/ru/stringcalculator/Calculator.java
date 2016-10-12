7f36e317772a80d32e45ebc3339a48a6e6eadce8
Pum8a <jonthor94@gmail.com>
2016-10-12 22:09:47 +0000

    Refactoring-made plusMultiNumb function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -5,17 +5,9 @@ public class Calculator {
 	public static int add(String text){
 		if(text.equals(""))
 			return 0;
-		/*else if(text.contains(",")){
-			String [] numbers = text.split(",");
-			return toInt(numbers[0]) + toInt(numbers[1]);
-		}*/
+		
 		else if(text.contains(",")) {
-			String [] numbers = text.split(",");
-			int sum = 0;
-			for(int i = 0; i < numbers.length;i++) {
-				sum += toInt(numbers[i]);
-			}
-			return sum;
+			return plusMultiNumb(text);
 		}
 
 		else
@@ -24,5 +16,16 @@ public class Calculator {
 
 	private static int toInt(String text) {
 		return Integer.parseInt(text);
+	}
+
+	private static int plusMultiNumb(String text) {
+		String [] numbers = text.split(",");
+		int sum = 0;
+
+		for(int i = 0; i < numbers.length;i++) {
+			sum += toInt(numbers[i]);
+		}
+
+		return sum;
 	}
 }
